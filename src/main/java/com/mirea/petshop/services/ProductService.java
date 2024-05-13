e7e9a53c66169fdf57f1827de6a4e202de825bd8
Sasha <66940823+SaSHa55555@users.noreply.github.com>
2024-05-13 20:14:32 +0300

    Update ProductService.java

--- src/main/java/com/mirea/petshop/services/ProductService.java
+++ src/main/java/com/mirea/petshop/services/ProductService.java
@@ -18,6 +18,12 @@ public class ProductService {
         this.iProductRepository = iProductRepository;
     }
 
+    /**
+    * Возвращает список всех товаров по определенному типу или список всех товаров.
+    *
+    * @param typeId идентификатор интересующего товара
+    * @return список товаров по id или всех товаров
+    */
     public List<Product> getAllProductsByTypeId(int typeId) {
         if (typeId != 0)
             return iProductRepository.findAllByTypeId(typeId);
@@ -25,10 +31,21 @@ public class ProductService {
             return getAllProducts();
     }
 
+    /**
+    * Возвращает список всех товаров.
+    *
+    * @return список товаров
+    */
     public List<Product> getAllProducts() {
         return iProductRepository.findAll();
     }
 
+    /**
+    * Возвращает товар с определенным id.
+    *
+    * @param id идентификатор интересующего товара
+    * @return Товар
+    */
     public Product getProductById(int id) {
         return iProductRepository.findById(id);
     }
